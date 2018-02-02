63d3b58ec82eae71def8887f523c84b1b3f9c2f7
Ishan Rayeen <ishan0445@gmail.com>
2018-02-02 16:39:11 +0530

    Added Android files as well

--- android-app/QRBasedAttendanceApp/app/src/main/java/com/ishan0445/qrbasedattendanceapp/MainActivity.java
+++ android-app/QRBasedAttendanceApp/app/src/main/java/com/ishan0445/qrbasedattendanceapp/MainActivity.java
@@ -1,13 +1,57 @@
 package com.ishan0445.qrbasedattendanceapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnScan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnScan = (Button) findViewById(R.id.btnScanQR);
+
+        btnScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+
+                    Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+                    intent.putExtra("SCAN_MODE", "QR_CODE_MODE"); // "PRODUCT_MODE for bar codes
+
+                    startActivityForResult(intent, 0);
+
+                } catch (Exception e) {
+
+                    Uri marketUri = Uri.parse("market://details?id=com.google.zxing.client.android");
+                    Intent marketIntent = new Intent(Intent.ACTION_VIEW,marketUri);
+                    startActivity(marketIntent);
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 0) {
+
+            if (resultCode == RESULT_OK) {
+                String contents = data.getStringExtra("SCAN_RESULT");
+
+                Toast.makeText(getApplicationContext(),"Scanned Data: "+ contents, Toast.LENGTH_LONG).show();
+            }
+            if(resultCode == RESULT_CANCELED){
+                Toast.makeText(getApplicationContext(),"Cancelled", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
