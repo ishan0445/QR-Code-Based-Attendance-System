d771f739da7da3d770ec08d71e0e0eca80f7852e
Ishan Rayeen <ishan0445@gmail.com>
2018-03-09 17:23:59 +0530

    Added POST call in Android App to mark attendance

---
+++ android-app/QRBasedAttendanceApp/app/src/main/java/com/ishan0445/qrbasedattendanceapp/Constants.java
@@ -0,0 +1,11 @@
+package com.ishan0445.qrbasedattendanceapp;
+
+/**
+ * Created by ishan on 09/03/18.
+ */
+
+public class Constants {
+    public static final String BASE_URL = "http://10.2.133.118:3000";
+    public static final String POST_ATTENDANCE_URL = BASE_URL + "/submitQRResponse";
+
+}
--- android-app/QRBasedAttendanceApp/app/src/main/java/com/ishan0445/qrbasedattendanceapp/MainActivity.java
+++ android-app/QRBasedAttendanceApp/app/src/main/java/com/ishan0445/qrbasedattendanceapp/MainActivity.java
@@ -1,12 +1,27 @@
 package com.ishan0445.qrbasedattendanceapp;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -45,9 +60,55 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == 0) {
 
             if (resultCode == RESULT_OK) {
-                String contents = data.getStringExtra("SCAN_RESULT");
 
-                Toast.makeText(getApplicationContext(),"Scanned Data: "+ contents, Toast.LENGTH_LONG).show();
+                final String qrCode = data.getStringExtra("SCAN_RESULT");
+                String url = Constants.POST_ATTENDANCE_URL;
+                TelephonyManager telephonyManager = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
+//                final String imeiCode = telephonyManager.getDeviceId();
+                final String imeiCode = "123434567812345";
+
+
+                StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                try {
+//                                    JSONObject jsonResponse = new JSONObject(response).getJSONObject("form");
+//                                    String site = jsonResponse.getString("site"),
+//                                            network = jsonResponse.getString("network");
+//                                    System.out.println("Site: "+site+"\nNetwork: "+network);
+
+                                    Toast.makeText(getApplicationContext(), response, Toast.LENGTH_LONG).show();
+
+
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                error.printStackTrace();
+                            }
+                        }
+                ) {
+                    @Override
+                    protected Map<String, String> getParams()
+                    {
+                        Map<String, String> params = new HashMap<>();
+                        // the POST parameters:
+                        params.put("roll", "20162115");
+                        params.put("imei", imeiCode);
+                        params.put("qrCode", qrCode);
+                        return params;
+                    }
+                };
+                Volley.newRequestQueue(this).add(postRequest);
+
+
+
+
             }
             if(resultCode == RESULT_CANCELED){
                 Toast.makeText(getApplicationContext(),"Cancelled", Toast.LENGTH_LONG).show();
