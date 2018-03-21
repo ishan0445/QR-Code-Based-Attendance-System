edfdb82d5b5dcdd7a7dd8b9bb4e6f1fad65d91b9
Ishan Rayeen <ishan0445@gmail.com>
2018-03-21 16:06:52 +0530

    Post request to send Attandance completed

--- android-app/QRBasedAttendanceApp/app/src/main/java/com/ishan0445/qrbasedattendanceapp/Constants.java
+++ android-app/QRBasedAttendanceApp/app/src/main/java/com/ishan0445/qrbasedattendanceapp/Constants.java
@@ -5,7 +5,9 @@ package com.ishan0445.qrbasedattendanceapp;
  */
 
 public class Constants {
-    public static final String BASE_URL = "http://10.2.133.118:3000";
+//    public static final String BASE_URL = "http://10.2.130.233:3000";       // Ishan
+    public static final String BASE_URL = "http://10.2.129.214:3000";     // Hemant
     public static final String POST_ATTENDANCE_URL = BASE_URL + "/submitQRResponse";
+//    public static final String POST_ATTENDANCE_URL = BASE_URL + "/test";
 
 }
--- android-app/QRBasedAttendanceApp/app/src/main/java/com/ishan0445/qrbasedattendanceapp/MainActivity.java
+++ android-app/QRBasedAttendanceApp/app/src/main/java/com/ishan0445/qrbasedattendanceapp/MainActivity.java
@@ -3,14 +3,17 @@ package com.ishan0445.qrbasedattendanceapp;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.TelephonyManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
@@ -20,8 +23,19 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -90,6 +104,7 @@ public class MainActivity extends AppCompatActivity {
                             @Override
                             public void onErrorResponse(VolleyError error) {
                                 error.printStackTrace();
+                                System.out.println("Here Comes the error!!!");
                             }
                         }
                 ) {
@@ -98,9 +113,11 @@ public class MainActivity extends AppCompatActivity {
                     {
                         Map<String, String> params = new HashMap<>();
                         // the POST parameters:
-                        params.put("roll", "20162115");
+                        params.put("rollNo", "20162115");
                         params.put("imei", imeiCode);
-                        params.put("qrCode", qrCode);
+                        params.put("QRCode", qrCode);
+                        Log.e("params",params.toString());
+
                         return params;
                     }
                 };
@@ -108,11 +125,108 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
+                // -------------------------------------------------------------------------------
+//                new SendPostRequest().execute();
             }
             if(resultCode == RESULT_CANCELED){
                 Toast.makeText(getApplicationContext(),"Cancelled", Toast.LENGTH_LONG).show();
             }
         }
     }
+
+
+
+//    public class SendPostRequest extends AsyncTask<String, Void, String> {
+//
+//        protected void onPreExecute(){}
+//
+//        protected String doInBackground(String... arg0) {
+//
+//            try {
+//
+//                URL url = new URL(Constants.POST_ATTENDANCE_URL); // here is your URL path
+//
+//                JSONObject postDataParams = new JSONObject();
+//                postDataParams.put("QRCode", "5aa286f1c52826470cead710");
+//                postDataParams.put("email", "abc@gmail.com");
+//                Log.e("params",postDataParams.toString());
+//
+//                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+//                conn.setReadTimeout(15000 /* milliseconds */);
+//                conn.setConnectTimeout(15000 /* milliseconds */);
+//                conn.setRequestMethod("POST");
+//                conn.setDoInput(true);
+//                conn.setDoOutput(true);
+//
+//                OutputStream os = conn.getOutputStream();
+//                BufferedWriter writer = new BufferedWriter(
+//                        new OutputStreamWriter(os, "UTF-8"));
+//                writer.write(getPostDataString(postDataParams));
+//
+//                writer.flush();
+//                writer.close();
+//                os.close();
+//
+//                int responseCode=conn.getResponseCode();
+//
+//                if (responseCode == HttpsURLConnection.HTTP_OK) {
+//
+//                    BufferedReader in=new BufferedReader(new
+//                            InputStreamReader(
+//                            conn.getInputStream()));
+//
+//                    StringBuffer sb = new StringBuffer("");
+//                    String line="";
+//
+//                    while((line = in.readLine()) != null) {
+//
+//                        sb.append(line);
+//                        break;
+//                    }
+//
+//                    in.close();
+//                    return sb.toString();
+//
+//                }
+//                else {
+//                    return new String("false : "+responseCode);
+//                }
+//            }
+//            catch(Exception e){
+//                return new String("Exception: " + e.getMessage());
+//            }
+//
+//        }
+//
+//        @Override
+//        protected void onPostExecute(String result) {
+//            Toast.makeText(getApplicationContext(), result,
+//                    Toast.LENGTH_LONG).show();
+//        }
+//    }
+
+//    public String getPostDataString(JSONObject params) throws Exception {
+//
+//        StringBuilder result = new StringBuilder();
+//        boolean first = true;
+//
+//        Iterator<String> itr = params.keys();
+//
+//        while(itr.hasNext()){
+//
+//            String key= itr.next();
+//            Object value = params.get(key);
+//
+//            if (first)
+//                first = false;
+//            else
+//                result.append("&");
+//
+//            result.append(URLEncoder.encode(key, "UTF-8"));
+//            result.append("=");
+//            result.append(URLEncoder.encode(value.toString(), "UTF-8"));
+//
+//        }
+//        return result.toString();
+//    }
 }
